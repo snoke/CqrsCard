e5cfd7f71fcb9ce9b0667712d5e945364cf3abd7
Stefan Sander <stowwel@gmail.com>
2023-12-20 04:29:06 +0700

    commit

--- assets/components/App.vue
+++ assets/components/App.vue
@@ -2,22 +2,6 @@
 <template>
   <div id="app">
 
-    <div id="cart">cart
-      <ul>
-        <li v-for="product in cart" :key="product.id" class="product">
-          <div class="name">{{ product.name }}</div>
-          <div class="price">price: {{ product.price }}€</div>
-          <div class="buttons">
-            <button type="button" class="btn btn-outline-secondary" @click="cartIncreaseProduct">+</button>
-            <button type="button" class="btn btn-outline-secondary" @click="cartDecreaseProduct">-</button>
-            <button type="button" class="btn btn-outline-secondary" @click="cartRemoveProduct">remove</button>
-          </div>
-        </li>
-      </ul>
-      <div class="checkout">
-        <button type="button" class="btn btn-outline-primary" @click="cartSave">save</button>
-      </div>
-    </div>
 
     <div id="products">products
       <ul>
