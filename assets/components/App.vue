b378d1e9edf8f79bf7775c623770afd615120efa
Stefan Sander <stowwel@gmail.com>
2023-12-20 02:46:48 +0700

    commit

--- assets/components/App.vue
+++ assets/components/App.vue
@@ -1,28 +1,54 @@
 <!-- Author: Stefan Sander <mail@stefan-sander.online> -->
 <template>
   <div id="app">
-    <Cart></Cart>
-    <Products></Products>
+
+    <div id="cart">cart
+      <ul>
+        <li v-for="product in cart" :key="product.id" class="product">
+          <div class="name">{{ product.name }}</div>
+          <div class="price">price: {{ product.price }}€</div>
+          <div class="buttons">
+            <button type="button" class="btn btn-outline-secondary" @click="cartIncreaseProduct">+</button>
+            <button type="button" class="btn btn-outline-secondary" @click="cartDecreaseProduct">-</button>
+            <button type="button" class="btn btn-outline-secondary" @click="cartRemoveProduct">remove</button>
+          </div>
+        </li>
+      </ul>
+      <div class="checkout">
+        <button type="button" class="btn btn-outline-primary" @click="cartSave">save</button>
+      </div>
+    </div>
+
+    <div id="products">products
+      <ul>
+        <li v-for="product in products" :key="product.id" class="product">
+          <div class="name">{{ product.name }}</div>
+          <div class="price">price: {{ product.price }}€</div>
+          <div class="add-to-cart">
+            <button type="button" class="btn btn-outline-primary" @click="productAddToCart(product)">add to cart
+            </button>
+          </div>
+        </li>
+      </ul>
+    </div>
   </div>
 </template>
 
 <script>
-import Products from './Products';
-import Cart from './Cart';
+//import Products from './Products';
+//import Cart from './Cart';
 import axios from 'axios'
 export default {
   name: 'App',
-  components: {Products,Cart},
+  //components: {Products,Cart},
   data: function() {
     return {
       cart: [],
       products: [],
     }
   },
-  created: function() {
-    this.$root.$on('productAddToCart', (product) => {
-      this.productAddToCart(product)
-    })
+  mounted: function() {
+    this.appGetProducts()
   },
   methods: {
     productAddToCart: function(product) {
