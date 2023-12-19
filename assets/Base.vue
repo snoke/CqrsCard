40cc86f585a263fa027db11bac6a42105336c20d
Stefan Sander <stowwel@gmail.com>
2023-12-19 15:37:12 +0700

    commit

--- assets/Base.vue
+++ assets/Base.vue
@@ -1,14 +1,8 @@
 <!-- Author: Stefan Sander <mail@stefan-sander.online> -->
 <template>
-  <div id="app"> 
-    <div v-if="!$root.connected">
-      connecting...
-    </div>
-    <div v-if="$root.connected">
-    <vue-confirm-dialog></vue-confirm-dialog>
+  <div id="app"> base
       <router-view></router-view>
     </div>
-  </div>
 </template>
 
 <style>
@@ -20,31 +14,6 @@ import App from './components/App';
 import Auth from './components/Auth';
 export default {
   name: 'Base',
-    components: {App,Auth},
-  data: function() {
-    return {
-    }
-  },
-  methods: {
-    connect() {
-      if (!this.$root.connection) {
-        this.$root.connect();
-        setTimeout(() => {
-            this.connect()
-        }, 3000)
-    }
-}
-  },
-  beforeDestroy () {
-    this.$root.$off('Base::connection-lost')
-  },
-  created: function() {
-        this.connect();
-      this.$root.$on('Base::connection-lost', () => {
-        this.connect();
-        });
-  },
-  updated: function() {
-  },
+    components: {App}
 }
 </script>
