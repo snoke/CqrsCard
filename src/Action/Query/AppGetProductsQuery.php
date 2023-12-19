5de39efc2b5224349103809baf3b28a9ea805e4c
Stefan Sander <stowwel@gmail.com>
2023-12-19 17:33:14 +0700

    commit

--- src/Action/Query/AppGetProductsQuery.php
+++ src/Action/Query/AppGetProductsQuery.php
@@ -15,7 +15,12 @@ class AppGetProductsQuery implements QueryInterface
 
     public function fetch(array $parameters = []): JsonResponse
     {
-        $e = AppGetProductsResource::get($this->productRepository->findAll());
+        try {
+            $e = AppGetProductsResource::get($this->productRepository->findAll());
+
+        } catch(\Throwablehrowable $e) {
+            var_dump($e->getMessage());
+        }
         die("ASDDDD");
         return new JsonResponse($e);
     }
