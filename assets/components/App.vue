6752e779af83e8205402af5e3c8d7e2550d899e5
Stefan Sander <stowwel@gmail.com>
2023-12-20 03:13:19 +0700

    commit

--- assets/components/App.vue
+++ assets/components/App.vue
@@ -1,64 +1,38 @@
 <!-- Author: Stefan Sander <mail@stefan-sander.online> -->
 <template>
   <div id="app">
-
-    <div id="cart">cart
-      <ul>
-        <li v-for="product in cart" :key="product.id" class="product">
-          <div class="name">{{ product.name }}</div>
-          <div class="price">price: {{ product.price }}€</div>
-          <div class="buttons">
-            <button type="button" class="btn btn-outline-secondary" @click="cartIncreaseProduct">+</button>
-            <button type="button" class="btn btn-outline-secondary" @click="cartDecreaseProduct">-</button>
-            <button type="button" class="btn btn-outline-secondary" @click="cartRemoveProduct">remove</button>
-          </div>
-        </li>
-      </ul>
-      <div class="checkout">
-        <button type="button" class="btn btn-outline-primary" @click="cartSave">save</button>
-      </div>
-    </div>
-
-    <div id="products">products
-      <ul>
-        <li v-for="product in products" :key="product.id" class="product">
-          <div class="name">{{ product.name }}</div>
-          <div class="price">price: {{ product.price }}€</div>
-          <div class="add-to-cart">
-            <button type="button" class="btn btn-outline-primary" @click="productAddToCart(product)">add to cart
-            </button>
-          </div>
-        </li>
-      </ul>
-    </div>
+    <Cart></Cart>
+    <Products></Products>
   </div>
 </template>
 
 <script>
-//import Products from './Products';
-//import Cart from './Cart';
+import Products from './Products';
+import Cart from './Cart';
 import axios from 'axios'
 export default {
   name: 'App',
-  //components: {Products,Cart},
+  components: {Products,Cart},
   data: function() {
     return {
       cart: [],
       products: [],
     }
   },
-  mounted: function() {
-    this.appGetProducts()
+  created: function() {
+    this.$root.$on('productAddToCart', (product) => {
+      this.productAddToCart(product)
+    })
   },
   methods: {
     productAddToCart: function(product) {
       this.cart.push(product);
     },
     appGetProducts: function() {
-      let self = this;
+      let parent = this;
       axios.get('/appGetProducts')
-          .then(function(response) {
-            this.products = response.data
+          .then(function (response) {
+            parent.products = Object.assign({}, response.data);
           })
           .catch(function (error) {
             console.log(error);
