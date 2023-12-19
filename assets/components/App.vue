93a3f2928d9aa7d1a7ab93313026c6e14d68649d
Stefan Sander <stowwel@gmail.com>
2023-12-20 04:14:23 +0700

    commit

--- assets/components/App.vue
+++ assets/components/App.vue
@@ -1,37 +1,22 @@
 <!-- Author: Stefan Sander <mail@stefan-sander.online> -->
 <template>
   <div id="app">
-    <Cart></Cart>
-    <Products></Products>
+    {{ test }}
   </div>
 </template>
 
 <script>
-import $ from "jquery";
-
-import Products from './Products';
-import Cart from './Cart';
-import axios from 'axios'
 export default {
   name: 'App',
-  components: {Products,Cart},
   data () {
     return {
-      cart: [],
-      products: [],
+      test: [],
     }
   },
+  mounted: () => {
+    this.test.push(1);
+},
   methods: {
-    productAddToCart(product) {
-      console.log(this.cart)
-      console.log(product)
-      this.cart.push(product)
-      alert("bbbb")
     },
-    appGetProducts: function() {
-      let parent = this;
-
-    },
-  },
 }
 </script>
