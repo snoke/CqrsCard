6906223a52cf25ed0155b6adfa3c50fa8a99d7cc
Stefan Sander <stowwel@gmail.com>
2023-12-20 02:05:05 +0700

    commit

--- assets/app.js
+++ assets/app.js
@@ -10,8 +10,7 @@ import Base from './Base';
 import App from './components/App';
 
 
-import { BootstrapVue,
-} from 'bootstrap-vue'
+import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
 
