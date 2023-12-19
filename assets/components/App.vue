6676fd615f40c8909bd5677ac80296d29e3a3434
Stefan Sander <stowwel@gmail.com>
2023-12-19 19:45:36 +0700

    commit

--- assets/components/App.vue
+++ assets/components/App.vue
@@ -2,7 +2,18 @@
 <template>
   <div id="app">
     <Cart></Cart>
-    <Products></Products>
+    <div id="products">
+      <ul>
+        <li v-for="product in products" :key="product.id" class="product">
+          <div class="name">{{ product.name }}</div>
+          <div class="price">price: {{ product.price }}€</div>
+          <div class="add-to-cart">
+            <button type="button" class="btn btn-outline-primary" @click="productAddToCart(product.id)">add to cart
+            </button>
+          </div>
+        </li>
+      </ul>
+    </div>
   </div>
 </template>
 
