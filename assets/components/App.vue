f12d16ae73ea951cb6a0798a885493daa5a7bef0
Stefan Sander <stowwel@gmail.com>
2023-12-20 05:48:36 +0700

    commit

--- assets/components/App.vue
+++ assets/components/App.vue
@@ -62,7 +62,7 @@ export default {
     },
     productAddToCart: function (index,product) {
       console.log(this.$refs)
-      for(let i = 0; i < this.$refs['amount_' + index]; i++) {
+      for(let i = 0; i < this.$refs['amount_' + index].value; i++) {
         this.cart = [product].concat(this.cart)
       }
       this.cart = [product].concat(this.cart)
