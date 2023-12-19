ef8dd2544f0f85430473279e24f8aab6b4b35dd4
Stefan Sander <stowwel@gmail.com>
2023-12-19 19:53:11 +0700

    commit

--- assets/components/App.vue
+++ assets/components/App.vue
@@ -1,28 +1,18 @@
 <!-- Author: Stefan Sander <mail@stefan-sander.online> -->
 <template>
-  <div id="app">
-    <Cart></Cart>
-    <div id="products">
-      <ul>
-        <li v-for="product in products" :key="product.id" class="product">
-          <div class="name">{{ product.name }}</div>
-          <div class="price">price: {{ product.price }}€</div>
-          <div class="add-to-cart">
-            <button type="button" class="btn btn-outline-primary" @click="productAddToCart(product.id)">add to cart
-            </button>
-          </div>
-        </li>
-      </ul>
-    </div>
+  <div id="app">a
+    <Cart></Cart>b
+    <Products></Products>
   </div>
 </template>
 
 <script>
 import axios from 'axios'
+import Products from './Products';
 import Cart from './Cart';
 export default {
   name: 'App',
-  components: {Cart},
+  components: {Products,Cart},
   data: function() {
     return {
       products: []
@@ -34,7 +24,6 @@ export default {
           .then(function (response) {
             this.products = JSON.parse(response.data);
             console.log(this.products);
-            alert(this.products[0].name)
           })
           .catch(function (error) {
             console.log(error);
---
+++ assets/components/Products.vue
@@ -0,0 +1,39 @@
+<template>
+  <div id="products">
+    <ul>
+      <li v-for="product in products" :key="product.id" class="product">
+        <div class="name">{{ product.name }}</div>
+        <div class="price">price: {{ product.price }}€</div>
+        <div class="add-to-cart">
+          <button type="button" class="btn btn-outline-primary" @click="productAddToCart(product.id)">add to cart
+          </button>
+        </div>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+import axios from 'axios'
+
+export default {
+  data: function () {
+    return {
+      products: [
+        {id: 1, name: 'test', price: 2.25}
+      ],
+    }
+  },
+  methods: {
+    productAddToCart: function (productId) {
+      axios.post('/' + 'addToCart', {
+        productId: productId,
+      })
+    }
+  },
+  mounted: function () {
+  },
+  updated: function () {
+  }
+}
+</script>
