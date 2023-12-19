f7c0d7251583bd3ea8d5fc680d3e718d919a08c9
Stefan Sander <stowwel@gmail.com>
2023-12-19 19:02:27 +0700

    commit

--- src/Entity/Cart.php
+++ src/Entity/Cart.php
@@ -18,12 +18,9 @@ class Cart
     #[ORM\Column]
     private ?int $sessionId = null;
 
-    #[ORM\ManyToMany(targetEntity: Product::class)]
-    private Collection $products;
 
     public function __construct()
     {
-        $this->products = new ArrayCollection();
     }
 
     public function getId(): ?int
@@ -51,19 +48,4 @@ class Cart
         return $this->products;
     }
 
-    public function addProduct(Product $product): static
-    {
-        if (!$this->products->contains($product)) {
-            $this->products->add($product);
-        }
-
-        return $this;
-    }
-
-    public function removeProduct(Product $product): static
-    {
-        $this->products->removeElement($product);
-
-        return $this;
-    }
 }
