65d456e7a828192733418f9e3fd8154698563dca
Stefan Sander <stowwel@gmail.com>
2023-12-20 04:24:18 +0700

    commit

--- assets/components/App.vue
+++ assets/components/App.vue
@@ -1,6 +1,22 @@
 <!-- Author: Stefan Sander <mail@stefan-sander.online> -->
 <template>
   <div id="app">
+    <div id="cart">cart
+      <ul>
+        <li v-for="product in cart" :key="product.id" class="product">
+          <div class="name">{{ product.name }}</div>
+          <div class="price">price: {{ product.price }}€</div>
+          <div class="buttons">
+            <button type="button" class="btn btn-outline-secondary" @click="cartIncreaseProduct">+</button>
+            <button type="button" class="btn btn-outline-secondary" @click="cartDecreaseProduct">-</button>
+            <button type="button" class="btn btn-outline-secondary" @click="cartRemoveProduct">remove</button>
+          </div>
+        </li>
+      </ul>
+      <div class="checkout">
+        <button type="button" class="btn btn-outline-primary" @click="cartSave">save</button>
+      </div>
+    </div>
 
     <div id="products">products
       <ul>
@@ -8,7 +24,7 @@
           <div class="name">{{ product.name }}</div>
           <div class="price">price: {{ product.price }}€</div>
           <div class="add-to-cart">
-            <button type="button" class="btn btn-outline-primary" @click="$parent.productAddToCart(product)">add to cart
+            <button type="button" class="btn btn-outline-primary" @click="productAddToCart(product)">add to cart
             </button>
           </div>
         </li>
@@ -23,20 +39,23 @@ import $ from "jquery";
 
 export default {
   name: 'App',
-  data () {
+  data() {
     return {
       products: [],
       card: []
     }
   },
-  mounted: function(){
+  mounted: function () {
     //this.test.push(1);
-    $.get( "/appGetProducts", ( response ) => {
+    $.get("/appGetProducts", (response) => {
       let data = JSON.parse(response);
       this.products = data;
     });
-},
+  },
   methods: {
-    },
+    productAddToCart: (product) => {
+      this.cart.push(product)
+    }
+  },
 }
 </script>
