fc38e0ceba4978fb524583b9c06bda4ec2090b8f
Stefan Sander <stowwel@gmail.com>
2023-12-20 04:04:32 +0700

    commit

--- assets/components/App.vue
+++ assets/components/App.vue
@@ -15,7 +15,7 @@ import axios from 'axios'
 export default {
   name: 'App',
   components: {Products,Cart},
-  data: function() {
+  data: () => {
     return {
       cart: [],
       products: [],
