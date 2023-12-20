77cc31f3cd23630640cebba72086850c74056b9f
Stefan Sander <stowwel@gmail.com>
2023-12-20 22:45:44 +0700

    commit

--- src/Cqrs/Query/AppGetProductsResource.php
+++ src/Cqrs/Query/AppGetProductsResource.php
@@ -4,17 +4,14 @@
  */
 namespace App\Cqrs\Query;
 
+use App\Cqrs\AbstractResource;
 use App\Entity\Product as ProductEntity;
 
-class AppGetProductsResource
+class AppGetProductsResource extends AbstractResource
 {
     public static function get(array $products) {
         return array_map(function(ProductEntity $product) {
-            return [
-                'id' => $product->getId(),
-                'name' => $product->getName(),
-                'price' => $product->getPrice(),
-            ];
+            return $this->serialize($product);
         }, $products);
     }
 }
