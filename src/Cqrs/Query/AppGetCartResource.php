a49f8bd5b4a69d64bc9d2bd753da14ab344da2cf
Stefan Sander <stowwel@gmail.com>
2023-12-20 22:30:25 +0700

    commit

--- src/Cqrs/Query/AppGetCartResource.php
+++ src/Cqrs/Query/AppGetCartResource.php
@@ -14,14 +14,13 @@ class AppGetCartResource extends AbstractResource
 {
     public  function get(?Cart $cart,?array $cartProducts)
     {
-        return $cart ? array_map(function (CartProduct $cartProduct) {
+        return $this->serialize($cart ? array_map(function (CartProduct $cartProduct) {
             $product = $cartProduct->getProduct();
-            return $this->serialize($product);
             return [
                 'id' => $product->getId(),
                 'name' => $product->getName(),
                 'price' => $product->getPrice(),
             ];
-        }, $cartProducts) : [];
+        }, $cartProducts) : []);
     }
 }
