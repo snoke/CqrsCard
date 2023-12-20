6da38e66be2d77aae8caa65ef477ad2ea40f40c0
Stefan Sander <stowwel@gmail.com>
2023-12-20 20:07:33 +0700

    commit

--- src/Entity/Command.php
+++ src/Entity/Command.php
@@ -7,6 +7,7 @@ use DateTime;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
 
+/** @MappedSuperclass */
 #[ORM\Entity(repositoryClass: CommandRepository::class)]
 class Command
 {
