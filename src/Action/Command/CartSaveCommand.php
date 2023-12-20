8077b3f4608e25a2fc5c84e8daf09c7f21d4d2e4
Stefan Sander <stowwel@gmail.com>
2023-12-20 17:27:21 +0700

    commit

--- src/Action/Command/CartSaveCommand.php
+++ src/Action/Command/CartSaveCommand.php
@@ -4,6 +4,7 @@ namespace App\Action\Command;
 use App\Entity\Cart;
 use App\Entity\CartProduct;
 use App\Repository\CartRepository;
+use App\Repository\CartProductRepository;
 use App\Repository\ProductRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\Request;
@@ -12,13 +13,15 @@ use Symfony\Component\HttpFoundation\RequestStack;
 class CartSaveCommand extends AbstractCommand implements CommandInterface
 {
     private CartRepository $cartRepository;
+    private CartProductRepository $cartProductRepository;
     private ProductRepository $productRepository;
 
-    public function __construct(EntityManagerInterface $entityManager, CartRepository $cartRepository, ProductRepository $productRepository) {
+    public function __construct(EntityManagerInterface $entityManager, CartRepository $cartRepository, CartProductRepository $cartProductRepository, ProductRepository $productRepository) {
 
         parent::__construct($entityManager);
 
         $this->cartRepository = $cartRepository;
+        $this->cartProductRepository = $cartProductRepository;
         $this->productRepository = $productRepository;
     }
 
@@ -28,8 +31,13 @@ class CartSaveCommand extends AbstractCommand implements CommandInterface
         var_dump(json_decode($requestStack->getCurrentRequest()->getContent(),true) );
 
         $sessionId = $requestStack->getSession()->get('sessionId');
-        foreach($this->cartRepository->findBy(['sessionId' => $sessionId]) as $_cart) {
-            $this->entityManager->remove($_cart);
+        foreach($this->cartRepository->findBy(['sessionId' => $sessionId]) as $cart) {
+
+            foreach($this->cartProductRepository->findBy(['cart' => $cart]) as $cartProduct) {
+
+                $this->entityManager->remove($cartProduct);
+            };
+            $this->entityManager->remove($cart);
         };
 
         $cart = new Cart();
