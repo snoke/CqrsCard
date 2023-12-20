57760ab2be7507aad39afccb17d4647a70005d68
Stefan Sander <stowwel@gmail.com>
2023-12-20 08:39:30 +0700

    commit

--- src/Action/Query/AppGetCardQuery.php
+++ src/Action/Query/AppGetCardQuery.php
@@ -1,8 +1,8 @@
 <?php
 namespace App\Action\Query;
 
-use App\Action\AbstractAction;
 use App\Repository\CartRepository;
+use App\Resources\AppGetCartResource;
 use App\Resources\AppGetProductsResource;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\JsonResponse;
