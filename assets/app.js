01c8b506c7b3961c129253f98a8f3ac8402fca06
Stefan Sander <stowwel@gmail.com>
2023-12-20 21:30:33 +0700

    commit

--- assets/app.js
+++ assets/app.js
@@ -10,35 +10,46 @@ import Base from './Base';
 import App from './components/App';
 
 
-import {BootstrapVue, IconsPlugin} from 'bootstrap-vue'
+import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
 
-import VueRouter from 'vue-router'
 
+import VueConfirmDialog from 'vue-confirm-dialog'
+Vue.use(VueConfirmDialog)
+Vue.component('vue-confirm-dialog', VueConfirmDialog.default)
+const moment = require('moment')
+Vue.use(require('vue-moment'), {
+  moment
+})
+import VueRouter from 'vue-router'
+Vue.use(VueRouter)
+var router = {
+  components: {Base},
+  routes: [
+      {
+              name: "app",
+              path: '/app',
+              component:  App,
+              props: true,
+              children:[
+              ]
+      }
+  ]
+};
+router = new VueRouter(router);
 Vue.use(BootstrapVue)
 Vue.use(IconsPlugin)
-
+import device from "vue-device-detector"
+Vue.use(device)
 var config = JSON.parse(document.getElementById('_symfonyData').innerHTML);
 console.log(config);
 new Vue({
-    data: function () {
+    data: function() {
         return {
-            config: config,
+            config:config,
         }
     },
     el: '#app',
-    render: h => h(Base),
-    router: new VueRouter({
-        components: {Base},
-        routes: [
-            {
-                name: "app",
-                path: '/app',
-                component: App,
-                props: true,
-                children: []
-            }
-        ]
-    }),
+    render: h => h(Base),  router: router,
 });
