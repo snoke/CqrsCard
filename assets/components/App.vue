8418a536a4ff32efa6c30767d1ffe4b3e26dbd6f
Stefan Sander <stowwel@gmail.com>
2023-12-20 20:01:15 +0700

    commit

--- assets/components/App.vue
+++ assets/components/App.vue
@@ -1,44 +1,54 @@
 <!-- Author: Stefan Sander <mail@stefan-sander.online> -->
 <template>
   <div id="app">
-    <div id="cart">cart
-      <ul>
-        <li v-for="(product,index) in this.cart" :key="product.id" class="product">
-          <div class="name">Name: {{ product.name }}</div>
-          <div class="price">Price: {{ product.price | currency }}</div>
-          <div class="buttons">
-            <button type="button" class="btn btn-outline-secondary" @click="cartIncreaseProduct(index,product)">+
-            </button>
-            <button type="button" class="btn btn-outline-secondary" @click="cartDecreaseProduct(index,product)">-
-            </button>
-            <button type="button" class="btn btn-outline-secondary" @click="cartRemoveProduct(index,product)">remove
-            </button>
+    <div class="row">
+
+      <div class="col-auto">
+
+        <div id="cart">cart
+          <ul>
+            <li v-for="(product,index) in this.cart" :key="product.id" class="product">
+              <div class="name">Name: {{ product.name }}</div>
+              <div class="price">Price: {{ product.price | currency }}</div>
+              <div class="buttons">
+                <button type="button" class="btn btn-outline-secondary" @click="cartIncreaseProduct(index,product)">+
+                </button>
+                <button type="button" class="btn btn-outline-secondary" @click="cartDecreaseProduct(index,product)">-
+                </button>
+                <button type="button" class="btn btn-outline-secondary" @click="cartRemoveProduct(index,product)">remove
+                </button>
+              </div>
+            </li>
+          </ul>
+          <div class="sum" style="text-align: center">Total: {{
+              cart.reduce((accumulator, object) => {
+                return accumulator + object['price'];
+              }, 0) | currency
+            }}
           </div>
-        </li>
-      </ul>
-      <div class="sum" style="text-align: center">Total: {{
-          cart.reduce((accumulator, object) => {
-            return accumulator + object['price'];
-          }, 0) | currency
-        }}
+          <button type="button" class="btn btn-outline-primary" @click="cartCheckout()">checkout
+          </button>
+        </div>
       </div>
-      <button type="button" class="btn btn-outline-primary" @click="cartCheckout()">checkout
-      </button>
+      <div class="col-auto">
+
+        <div id="products">products
+          <ul>
+            <li v-for="(product,index) in products" :key="product.id" class="product">
+              <div class="name">{{ product.name }}</div>
+              <div class="price">price: {{ product.price | currency }}</div>
+              <div class="add-to-cart">
+                <input type="text" :ref="'amount_'+index" value="1"/>
+                <button type="button" class="btn btn-outline-primary" @click="productAddToCart(index,product)">add to cart
+                </button>
+              </div>
+            </li>
+          </ul>
+        </div>
+      </div>
+
     </div>
 
-    <div id="products">products
-      <ul>
-        <li v-for="(product,index) in products" :key="product.id" class="product">
-          <div class="name">{{ product.name }}</div>
-          <div class="price">price: {{ product.price | currency }}</div>
-          <div class="add-to-cart">
-            <input type="text" :ref="'amount_'+index" value="1"/>
-            <button type="button" class="btn btn-outline-primary" @click="productAddToCart(index,product)">add to cart
-            </button>
-          </div>
-        </li>
-      </ul>
-    </div>
   </div>
 
 </template>
