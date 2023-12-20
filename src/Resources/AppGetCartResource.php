c39ed944723ff3cdb2a4f4cfb533555d4fcf559d
Stefan Sander <stowwel@gmail.com>
2023-12-20 08:53:50 +0700

    commit

--- src/Resources/AppGetCartResource.php
+++ src/Resources/AppGetCartResource.php
@@ -1,4 +1,5 @@
 <?php
+
 namespace App\Resources;
 
 use App\Entity\Cart as Cart;
@@ -7,13 +8,14 @@ use Doctrine\ORM\EntityManagerInterface;
 
 class AppGetCartResource
 {
-    public static function get(Cart $cart) {
-        return array_map(function(ProductEntity $product) {
+    public static function get(?Cart $cart)
+    {
+        return $cart ? array_map(function (ProductEntity $product) {
             return [
                 'id' => $product->getId(),
                 'name' => $product->getName(),
                 'price' => $product->getPrice(),
             ];
-        }, $cart->getProducts()->toArray());
+        }, $cart->getProducts()->toArray()) : [];
     }
 }
