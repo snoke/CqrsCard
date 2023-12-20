fb89e5187381e7c4d47ef7f3a5907f5219183b27
Stefan Sander <stowwel@gmail.com>
2023-12-20 20:26:43 +0700

    commit

--- src/Cqrs/Command/CartSaveCommandHandlerHandler.php
+++ src/Cqrs/Command/CartSaveCommandHandlerHandler.php
@@ -53,8 +53,8 @@ class CartSaveCommandHandlerHandler extends AbstractCommandHandler implements Co
             $cartProduct->setProduct($entity);
             $this->entityManager->persist($cartProduct);
         }
-        $this->entityManager->persist($command);
-        //$this->entityManager->persist(new Command($command->getSessionId(),json_encode($command->getProducts())));
+        //$this->entityManager->persist($command);
+        $this->entityManager->persist(new Command($command->getSessionId(),json_encode($command->getProducts())));
         $this->entityManager->flush();
         return true;
     }
